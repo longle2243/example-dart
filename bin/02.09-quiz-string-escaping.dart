eb3b952daf70d3915952e9ed5835c53950b84085
Quang Long <longle2243@gmail.com>
2022-07-14 16:15:32 +0700

    quiz

--- bin/02.09-quiz-string-escaping.dart
+++ bin/02.09-quiz-string-escaping.dart
@@ -4,10 +4,4 @@ void main() {
   print('$a'); //in gia tri bien $a
   print('\$a'); //in ki tu dac biet $ va chu cai a
   print('\$$a'); //in ki tu dac biet $ va gia tri bien $a
-
-  //Increment and decrement
-  int x = 1;
-  int y = x++;  //gan y=x va x=x+1
-  int z = --y;  //y=y-1 va gan z=y
-  print('x: $x, y: $y, z: $z');
 }
---
+++ bin/02.18-quiz-increment-decrement.dart
@@ -0,0 +1,7 @@
+void main() {
+  //Increment and decrement
+  int x = 1;
+  int y = x++; //gan y=x va x=x+1
+  int z = --y; //y=y-1 va gan z=y
+  print('x: $x, y: $y, z: $z');
+}
